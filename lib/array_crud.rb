95b3faab24542b6d496b3376674fad1d89e03a80
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-09 06:22:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   my_array = []
 end
 
-def add_element_to_end_of_array(array, element = [1,2,3])
-  element << 4
+def add_element_to_end_of_array(array, element)
+  element << 1, 2, 3, 4
   puts elements.inspect
 end
 
