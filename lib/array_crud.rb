3f096a4d4f43f4d02a4bd4da7e8d4f034eaa2b71
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-09 06:26:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = [1, 2, 3]
+  my_array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << 4
+  create_an_array << 5
   puts create_an_array.inspect
 end
 
