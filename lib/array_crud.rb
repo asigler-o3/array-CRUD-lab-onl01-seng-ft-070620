6c1f8ddfbb0f892064815c328c77b1079a309a12
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-09 07:33:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
   my_array = [1, 2, 3, 4]
 end
 
-def add_element_to_end_of_array(array= ["wow", "I", "am", "really", "learning"], element= "arrays!")
+def add_element_to_end_of_array(array, element)
   array= ["wow", "I", "am", "really", "learning"]
+  element= "arrays!"
   array.push element
   puts array.inspect
 end
