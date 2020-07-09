bee276e57bb3f2df34ab189975c485517e39ccc4
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-09 07:02:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   element.unshift ("arrays!")
-  puts array.inspect
+  puts element.inspect
 end
 
 def remove_element_from_end_of_array(array)
